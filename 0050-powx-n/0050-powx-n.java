dce6ba25702e46f0d0c105fb1f08b05feb3e292f
Rishu1790 <luvkush9534901790@gmail.com>
2026-03-29 04:32:34 +0530

    Time: 0 ms (100%), Space: 48.1 MB (42.59%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;        
+
+        if(N < 0){       
+            x = (1/x);
+            N = -N;         
+        }
+        if(N == 0) return 1;
+        if(N == 1) return x;
+
+        double t = myPow(x, (int)(N/2)); 
+        double hbs = t * t;
+
+        if(N % 2 != 0){  
+            hbs = x * hbs;
+        }
+        return hbs;
+    }
+}
