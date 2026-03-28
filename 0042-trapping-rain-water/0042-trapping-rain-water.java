3b8b77ceee10d6067ca10919421d1fbf5ca7dfa4
Rishu1790 <luvkush9534901790@gmail.com>
2026-03-29 04:33:23 +0530

    Time: 0 ms (100%), Space: 48 MB (38.88%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int trap(int[] height) {
+       if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // If current height is greater than leftMax, update leftMax
+                // Otherwise, we can trap water because we know height[right] is taller
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                // If current height is greater than rightMax, update rightMax
+                // Otherwise, trap water based on the right boundary
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+}
