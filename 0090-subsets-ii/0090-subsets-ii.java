78dde36f3415a755b41cb577901a0283f2d053d1
Rishu Ranjan <luvkush9534901790@gmail.com>
2026-09-03 18:00:16 +0530

    Time: 2 ms (99.72%), Space: 45 MB (76.7%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,25 +1,31 @@
 class Solution {
-    private static void findSubsets(int idx,int nums[],List<List<Integer>> ans, List<Integer> ds){
-        
-            ans.add(new ArrayList<>(ds));
-          
 
-          for(int i = idx;i<nums.length;i++){
-            if(i!=idx && nums[i]==nums[i-1]) continue;
-            ds.add(nums[i]);
+static List<List<Integer>> solve(int[] nums,List<List<Integer>> result, List<Integer> ds,int i){
+    if(i >= nums.length ){
+        result.add(new ArrayList<>(ds));
+        return result;
+    }
 
-            findSubsets(i+1,nums,ans,ds);
+  
 
-            ds.remove(ds.size()-1);
-          }
- }
-    
+    ds.add(nums[i]);
+    solve(nums,result,ds,i+1);
+    ds.remove(ds.size()-1);
+      while(i+1< nums.length && nums[i]==nums[i+1]){
+     i++;
+    }
+
+solve(nums,result,ds,i+1);
+    return result;
+}
+
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        findSubsets(0,nums,ans,new ArrayList<>());
+           List<List<Integer>> result = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        solve(nums,result,ds,0);
 
-        return ans;
-        
+        return result;
     }
 }
